50a0260caebd38b0428db7b0120f057224469294
Carmen Liu <carmenl@orionhealth.com>
2017-07-30 21:03:33 +1200

    add config information

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    FB_PAGE_TOKEN: '',
-    FB_VERIFY_TOKEN: '',
+    FB_PAGE_TOKEN: 'EAAb34sZC5AQYBABJKENZBZAy4eB1vjGxc90QXNp87qvyXaCvhVhr4YbdnYwi57PG6epv80bAhZA4Fz7OUw8ZCpgPoKu1lWwsGDRhcZCVuHNL2CoMLtDpMOynNaXOUYjhauMZAW5FvoACl3pVZCkMCX5fTZBrlZALbqNfnYQ6qomzcUZBgZDZD',
+    FB_VERIFY_TOKEN: 'iamninjasbotcaesar',
     API_AI_CLIENT_ACCESS_TOKEN: '',
     FB_APP_SECRET: '',
-    SERVER_URL: "",
+    SERVER_URL: "https://ninjacaesar.herokuapp.com/",
 };
