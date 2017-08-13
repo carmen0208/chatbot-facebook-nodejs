1212eaacaf2ee505f88d5b562a9bc76d00af4689
Carmen Liu <carmen.liu@orionhealth.com>
2017-08-13 19:25:59 +1200

    fix bugs

--- app.js
+++ app.js
@@ -209,7 +209,7 @@ function handleApiAiAction(sender, action, responseText, contexts, parameters) {
 				}, function(error, response, body) {
 					if(!error && response.statusCode == 200) {
 						let weather = JSON.parse(body);
-						console.log(JSON.stringify(weather);
+						console.log(JSON.stringify(weather));
 						if(weather.hasOwnProperty['weather']) {
 							let reply = `${responseText} ${weather["weather"][0]["description"]}`;
 							sendTextMessage(sender, reply);
